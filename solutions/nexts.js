002c55b2005f126b36fa0171d6d0aa467079b5bb
Denis Likov <denismassters@gmail.com>
2015-07-13 21:29:29 +0300

    just functional

---
+++ solutions/nexts.js
@@ -0,0 +1,20 @@
+/* global module require */
+
+// Создайте файл с именем <github nickname>.js и скопируйте туда содержимое этого файла.
+// Напишите собственные реализации этих функций в соответствии с README
+
+var l = require("lodash");
+
+module.exports = {
+  imperative: function(str) { "use strict";
+    return l.words(str).length;
+  },
+
+  oop: function(str) { "use strict";
+    return l.words(str).length;
+  },
+
+  functional: function(str) { "use strict";
+    return str.split(/[^a-zA-Z]/).filter(Boolean).length;
+  }
+};
